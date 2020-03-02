e4dfd56213eb4f0d495e956ba1a2f549012b1b7d
Sean Maden <metamaden@users.noreply.github.com>
2020-03-02 09:44:12 -0800

    updates man

--- R/rmpipeline.R
+++ R/rmpipeline.R
@@ -30,21 +30,21 @@ getblocks <- function(slength, bsize){
 }
 
 #' Append metadata to HDF5 or SE object
-#' 
+#'
 #' Passes version info and timestamp from Python to object metadata
 #' @param title Object title
 #' @param version Numeric version to be passed, should conform to ##.##.## nomenclature
 #' @param pname Name of pipeline package (default: "rmpipeline")
 #' @param sname Name of Python script (default: "get_timestamp.y")
 #' @return Metadata content for the object
-#' @export 
-get.metadata <- function(title, version, pname = "rmpipeline", 
+#' @export
+get_metadata <- function(title, version, pname = "rmpipeline",
                          sname = "get_timestamp.py"){
   mdl <- list(title = title, version = version)
   # get timestamp from package python script
-  path <- paste(system.file(package = pname), 
+  path <- paste(system.file(package = pname),
                 sname, sep="/")
-  command <- paste("python", path, sname, 
+  command <- paste("python", path, sname,
                    "to", sep = " ")
   mdl[["timestamp"]] <- system(command, intern = T)
   return(mdl)
@@ -55,10 +55,10 @@ get.metadata <- function(title, version, pname = "rmpipeline",
 #----------------------------
 
 #' Make data tables from IDATs
-#' 
-#' Make data tables from IDATs, including red and green channel signals and noob-normalized Beta-values. 
+#'
+#' Make data tables from IDATs, including red and green channel signals and noob-normalized Beta-values.
 #' This function should be run from the "recount-methylation" base directory.
-#' 
+#'
 #' @param version Version of the run for data table filenames.
 #' @param verbose Whether to return verbose notifications.
 #' @param gsmint Number of GSMs to process at a time, typically runs best near 50 samples.
@@ -68,16 +68,16 @@ get.metadata <- function(title, version, pname = "rmpipeline",
 #' @return Creates new HDF5 database from DNAm signal data.
 #' @export
 
-h5db.fromsignal <- function(version, verbose = TRUE, gsmint = 60,
+h5db_fromsignal <- function(version, verbose = TRUE, gsmint = 60,
                             fnstem = "mdat.compilation", sepval = " ", getnb = FALSE,
                             idats.path = paste("recount-methylation-files", "idats", sep = "/"),
-                            dest.path = paste("recount-methylation-analysis", 
+                            dest.path = paste("recount-methylation-analysis",
                                               "files", "mdata", "compilations", sep = "/")){
-  
+
   # get run metadata
   runmd <- get.metadata(version, "notitle")
   nts = runmd[["timestamp"]]
-  
+
   # get valid gsms idats dir
   idats.lf = list.files(idats.path)
   which.valid1 = grepl("\\.idat$", substr(idats.lf, nchar(idats.lf) - 4, nchar(idats.lf)))
@@ -86,7 +86,7 @@ h5db.fromsignal <- function(version, verbose = TRUE, gsmint = 60,
   gsmu = gsub("\\..*", "", idats.valid)
   gsmu = gsmu[grepl("^GSM.*", gsmu)]
   gsmu = unique(gsmu)
-  
+
   # determine target idats
   # note: add max timestamps filter
   gpath = c()
@@ -112,20 +112,20 @@ h5db.fromsignal <- function(version, verbose = TRUE, gsmint = 60,
     }
   }
   gsmii = seq(1, length(gsmu), gsmint)
-  
+
   # make new data paths
-  reds.fn <- paste(paste("redsignal", nts, version, sep = "_"), 
+  reds.fn <- paste(paste("redsignal", nts, version, sep = "_"),
                    fnstem, sep = ".")
-  grns.fn <- paste(paste("greensignal", nts, version, sep = "_"), 
+  grns.fn <- paste(paste("greensignal", nts, version, sep = "_"),
                    fnstem, sep = ".")
   reds.path = paste(destpath, reds.fn, sep = "/")
   grns.path = paste(destpath, grns.fn, sep = "/")
   if(getnb){
-    nb.fn <- paste(paste("noobbeta", nts, version, sep = "_"), 
+    nb.fn <- paste(paste("noobbeta", nts, version, sep = "_"),
                    fnstem, sep = ".")
     nb.path = paste(destpath, nb.fn, sep = "/")
   }
-  
+
   # instantiate new empty data tables
   cn = c("gsmi")
   rgi = minfi::read.metharray(c(paste(idats.path, gpath[1:2], sep = "/")))
@@ -140,7 +140,7 @@ h5db.fromsignal <- function(version, verbose = TRUE, gsmint = 60,
   if(getnb){
     fwrite(nbcn, nb.path, sep = sepval, append = FALSE)
   }
-  
+
   # append new methdata
   tt = Sys.time()
   for(i in 1:length(gsmii)){
@@ -148,24 +148,24 @@ h5db.fromsignal <- function(version, verbose = TRUE, gsmint = 60,
     # read in new data
     pathl = paste(idats.path, gpath[gi:(gi + gsmint - 1)], sep = "/")
     rgi = minfi::read.metharray(c(pathl))
-    
+
     # get data matrices
     redi = matrix(c(colnames(rgi), t(getRed(rgi))), ncol = nrow(rgi) + 1)
     grni = matrix(c(colnames(rgi), t(getGreen(rgi))), ncol = nrow(rgi) + 1)
-    
+
     # append new data
     data.table::fwrite(redi, reds.path, sep = sepval, append = TRUE)
     data.table::fwrite(grni, grns.path, sep = sepval, append = TRUE)
-    
+
     # parse noob-normalized data option
     if(getnb){
       gsi = preprocessNoob(rgi)
       nbi = matrix(c(colnames(gsi), t(getBeta(rgi))), ncol = nrow(gsi) + 1)
       data.table::fwrite(nbi, grns.path, sep = sepval, append = TRUE)
     }
-    
+
     if(verbose){
-      message("Finished gsmi ", gi, " to ", gi + gsmint - 1, 
+      message("Finished gsmi ", gi, " to ", gi + gsmint - 1,
               ", time : ", Sys.time() - tt)
     }
   }
@@ -177,28 +177,28 @@ h5db.fromsignal <- function(version, verbose = TRUE, gsmint = 60,
 #------------------------------------
 
 #' Append metadata to HDF5 or SE object
-#' 
+#'
 #' Add signal data (red and green channel) to the HDF5 database.
 #' @param dbn Name of the HDF5 database targeted.
 #' @param fnl List of signal tables containing data to be added.
-#' @param dsnl List of data set names in HDF5 database to be populated. 
+#' @param dsnl List of data set names in HDF5 database to be populated.
 #' @param rmax Total rows to append to data sets, reflecting total samples.
 #' @param cmax Total columns to append to data sets, reflecting total assays or probes.
 #' @param verbose Whether to print verbose progress messages.
 #' @param nr.inc Number of samples to append at a time (default: 10).
 #' @return Populates the HDF5 database
-#' @export 
-h5.addtables = function(dbn, fnl, dsnl, rmax, cmax, 
+#' @export
+h5_addtables = function(dbn, fnl, dsnl, rmax, cmax,
                     verbose = TRUE, nr.inc = 10){
   for(di in 1:length(dsnl)){
     fnread = fnl[di]; dsn = dsnl[di]
-    h5createDataset(dbn, dsn, dims = c(rmax, cmax), 
-                    maxdims = c(H5Sunlimited(), H5Sunlimited()), 
+    h5createDataset(dbn, dsn, dims = c(rmax, cmax),
+                    maxdims = c(H5Sunlimited(), H5Sunlimited()),
                     storage.mode = "double", level = 5, chunk = c(1, 5))
     rn = cn = c()
     con <- file(fnread, "r")
     cn = unlist(strsplit(readLines(con, n = 1), " "))
-    cn = cn[2:length(cn)] # filt first value 
+    cn = cn[2:length(cn)] # filt first value
     cn = gsub("\n", "",gsub('\"', '', cn[1:cmax])) # grab the max indexed value
     nri = seq(1, rmax, nr.inc)
     tt <- Sys.time()
@@ -219,20 +219,20 @@ h5.addtables = function(dbn, fnl, dsnl, rmax, cmax,
       }
       rn = c(rn, wgsm) # gsm ids for new data
       class(dff) = "numeric"
-      h5write(dff, file = dbn, name = dsn, 
+      h5write(dff, file = dbn, name = dsn,
               index = list(j:(j + nrow(dff) - 1), 1:cmax))
       h5closeAll()
       j = j + nrow(dff) # start index for next dff
-      message("For ds ", dsn,", finished reading index ", i, " to ", i + (nr.inc - 1), 
+      message("For ds ", dsn,", finished reading index ", i, " to ", i + (nr.inc - 1),
               ", time; ", Sys.time() - tt)
     }
     message("Adding row and column names for ds ", dsn)
     cnn = paste0(dsn, ".colnames"); rnn = paste0(dsn, ".rownames");
-    h5createDataset(dbn, cnn, dims = length(cn), maxdims = c(H5Sunlimited()), 
+    h5createDataset(dbn, cnn, dims = length(cn), maxdims = c(H5Sunlimited()),
                     storage.mode = "character", level = 5, # compression level, 1-9
                     chunk = c(20), size = 256) # chunk dims
     message("Added colnames...")
-    h5createDataset(dbn, rnn, dims = length(rn), maxdims = c(H5Sunlimited()), 
+    h5createDataset(dbn, rnn, dims = length(rn), maxdims = c(H5Sunlimited()),
                     storage.mode = "character", level = 5, # compression level, 1-9
                     chunk = c(20), size = 256) # chunk dims
     message("Added rownames...")
@@ -259,7 +259,7 @@ h5.addtables = function(dbn, fnl, dsnl, rmax, cmax,
 #' @param ncol.chunk Number of columns (GSMs/samples) per chunk in saved h5 datasets.
 #' @return Adds new minfi h5 datasets to specified h5 db (dbn).
 #' @export
-h5.newtables <- function(dbn, dsn.nb = "noobbeta",
+h5_newtables <- function(dbn, dsn.nb = "noobbeta",
                          dsn.meth = "methylated_signal",
                          dsn.unmeth = "unmethylated_signal",
                          dsn.red = "redsignal", dsn.grn = "greensignal",
@@ -280,24 +280,24 @@ h5.newtables <- function(dbn, dsn.nb = "noobbeta",
   newdims <- c(length(rs.rn), ncg)
   chunkvars <- c(5, ncol.chunk)
   # make new tables
-  rhdf5::h5createDataset(dbn, "unmethylated_signal", dims = newdims, 
-                         maxdims = c(H5Sunlimited(), H5Sunlimited()), 
+  rhdf5::h5createDataset(dbn, "unmethylated_signal", dims = newdims,
+                         maxdims = c(H5Sunlimited(), H5Sunlimited()),
                          storage.mode = "double", level = 5, chunk = chunkvars)
-  rhdf5::h5createDataset(dbn, "methylated_signal", dims = newdims, 
-                         maxdims = c(H5Sunlimited(), H5Sunlimited()), 
+  rhdf5::h5createDataset(dbn, "methylated_signal", dims = newdims,
+                         maxdims = c(H5Sunlimited(), H5Sunlimited()),
                          storage.mode = "double", level = 5, chunk = chunkvars)
-  rhdf5::h5createDataset(dbn, "noobbeta", dims = newdims, 
-                         maxdims = c(H5Sunlimited(), H5Sunlimited()), 
+  rhdf5::h5createDataset(dbn, "noobbeta", dims = newdims,
+                         maxdims = c(H5Sunlimited(), H5Sunlimited()),
                          storage.mode = "double", level = 5, chunk = chunkvars)
   # append new data
   t1 <- Sys.time()
   for(i in 1:length(sbv)){
     b = sbv[[i]]
     gsmvi <- gsub("\\..*", "", rs.rn[b])
-    se.rgi = recountmethylation::getrg(gsmv = gsmvi, 
-                                       cgv = "all", dbn = dbn, 
-                                       data.type = "se", 
-                                       metadata = FALSE, 
+    se.rgi = recountmethylation::getrg(gsmv = gsmvi,
+                                       cgv = "all", dbn = dbn,
+                                       data.type = "se",
+                                       metadata = FALSE,
                                        verbose = FALSE)
     # new se and data objects
     se.nb <- preprocessNoob(se.rgi)
@@ -306,15 +306,15 @@ h5.newtables <- function(dbn, dsn.nb = "noobbeta",
     nb <- t(minfi::getBeta(se.nb))
     # append new data to h5 data
     writei <- list(b[1]:b[length(b)], 1:ncg)
-    rhdf5::h5write(unmethb, file = dbn, 
+    rhdf5::h5write(unmethb, file = dbn,
                    name = dsn.unmeth, index = writei)
-    rhdf5::h5write(methb, file = dbn, 
+    rhdf5::h5write(methb, file = dbn,
                    name = dsn.meth, index = writei)
-    rhdf5::h5write(nb, file = dbn, 
+    rhdf5::h5write(nb, file = dbn,
                    name = dsn.nb, index = writei)
     if(verbose){
-      message("iter: ", i," of ", 
-              length(sbv),", time: ", 
+      message("iter: ", i," of ",
+              length(sbv),", time: ",
               Sys.time() - t1)
     }
   }
@@ -323,17 +323,17 @@ h5.newtables <- function(dbn, dsn.nb = "noobbeta",
 }
 
 #' Make and populate a new HDF5 database
-#' 
+#'
 #' Add signal data (red and green channel) to the HDF5 database.
 #' @param dbn Name of the HDF5 database targeted.
 #' @param fnl List of signal tables containing data to be added.
-#' @param dsnl List of data set names in HDF5 database to be populated. 
+#' @param dsnl List of data set names in HDF5 database to be populated.
 #' @param rmax Total rows to append to data sets, reflecting total samples.
 #' @param cmax Total columns to append to data sets, reflecting total assays or probes.
 #' @param newtables Whether to also add new data tables (noob-norm. Beta-values, meth. and unmeth. signal).
 #' @return Populates the HDF5 database
-#' @export 
-make.h5db <- function(dbn, fnl, rmax = 35500, cmax = 622399, newtables = TRUE,
+#' @export
+make_h5db <- function(dbn, fnl, rmax = 35500, cmax = 622399, newtables = TRUE,
                  dsnl = c("redsignal", "greensignal", "noobbeta")){
   try(rhdf5::h5createFile(dbn))
   # remove old data if present
@@ -346,12 +346,12 @@ make.h5db <- function(dbn, fnl, rmax = 35500, cmax = 622399, newtables = TRUE,
   if(verbose){message("Adding and populating data tables to HDF5 database")}
   h5ds.add(fnl = fnl, dsnl = dsnl, rmax = rmax, cmax = cmax, nr.inc = nr.inc)
   if(verbose){message("Finished adding red and green channel data to h5 databse.")}
-  
+
   if(newtables){
     if(verbose){message("Adding new data tables.")}
     h5.newtables(dbn)
   }
-  
+
   if(verbose){message("Finished all processes. Returning.")}
   return(NULL)
 }
@@ -368,7 +368,7 @@ make.h5db <- function(dbn, fnl, rmax = 35500, cmax = 622399, newtables = TRUE,
 #' @param se SummarizedExperiment object.
 #' @return SummarizedExperiment object with appended phenotype data.
 #' @export
-se.addpheno <- function(mdp, se){
+se_addpheno <- function(mdp, se){
   # Adds pheno data to a SummarizedExperiment objects
   mdp <- mdp[mdp$basename %in% colnames(se),]
   bnv <- colnames(se)
@@ -381,7 +381,7 @@ se.addpheno <- function(mdp, se){
   mdp.ov <- matrix(c(gsm.ov,
                      rep(nacol, 6),
                      bnv.ov,
-                     rep(nacol, 11)), 
+                     rep(nacol, 11)),
                    nrow = length(gsm.ov))
   colnames(mdp.ov) <- colnames(mdp)
   mdp <- rbind(mdp, mdp.ov)
@@ -408,17 +408,17 @@ se.addpheno <- function(mdp, se){
 #' @param se Type of SummarizedExperiment to parse (either rg for RGChannelSet, gr for GenomicRatioSet, or gm for GenomicMethylSet).
 #' @param verbose Whether to show verbose status messages.
 #' @param dsn.cn Name of h5 db object with column names (cgids or assays, optional).
-#' @param dbn Name or path to h5 db. 
+#' @param dbn Name or path to h5 db.
 #' @param replace.opt Whether to replace/overwrite any existing H5 SE directories of the same name as `newfn`.
 #' @return List of index blocks of min length `slength`/`bsize`
 #' @export
-make.h5se <- function(newfn, dsn.data1, dsn.data2 = NULL, 
-                      mdpost, dsn.rn = "redsignal.rownames", 
+make_h5se <- function(newfn, dsn.data1, dsn.data2 = NULL,
+                      mdpost, dsn.rn = "redsignal.rownames",
                       se = c("rg", "gr", "gm"), verbose = TRUE,
                       dsn.cn = FALSE, dbn = "remethdb2.h5", replace.opt = TRUE){
   # Sets up SummarizedExperiment creation from h5 file
   # Stores SE object to SE H5 file with DelayedArray processing
-  
+
   # anno for se sets
   anno = c("IlluminaHumanMethylation450k", "ilmn12.hg19")
   names(anno) = c("array", "annotation")
@@ -444,7 +444,7 @@ make.h5se <- function(newfn, dsn.data1, dsn.data2 = NULL,
     rn <- rhdf5::h5read(dbn, dsn.rn)
     rownames(nb) <- as.character(rn)
     nb <- t(nb)
-    if(!identical(nrow(nb), nrow(ldat[[1]])) | 
+    if(!identical(nrow(nb), nrow(ldat[[1]])) |
        !identical(ncol(nb), ncol(ldat[[1]]))){
       stop("Matrix dsn.data2 not similar dim to dsn.data1!")
     }
@@ -454,7 +454,7 @@ make.h5se <- function(newfn, dsn.data1, dsn.data2 = NULL,
   }
   # get probe ids or addresses
   if(!dsn.cn){
-    man = eval(parse(text = paste("IlluminaHumanMethylation450kanno.ilmn12.hg19", 
+    man = eval(parse(text = paste("IlluminaHumanMethylation450kanno.ilmn12.hg19",
                                   "Manifest", sep = "::")))
     cgrn <- rownames(man)
     ldat <- lapply(ldat, function(x){
@@ -462,7 +462,7 @@ make.h5se <- function(newfn, dsn.data1, dsn.data2 = NULL,
       return(x)
     })
     if(!se == "rg"){
-      ordergr <- order(match(names(grcg), 
+      ordergr <- order(match(names(grcg),
                              rownames(ldat[[1]])))
       grcg <- grcg[ordergr]
       if(!identical(rownames(ldat[[1]]), names(grcg)) |
@@ -479,8 +479,8 @@ make.h5se <- function(newfn, dsn.data1, dsn.data2 = NULL,
   if(verbose){message("Making the new se object...")}
   if(se == "rg"){
     if(verbose){message("Making RGChannelSet...")}
-    gri <- minfi::RGChannelSet(Red = ldat[[1]], 
-                               Green = ldat[[2]], 
+    gri <- minfi::RGChannelSet(Red = ldat[[1]],
+                               Green = ldat[[2]],
                                anno = anno)
     metadata(gri) <- get_metadata(paste0("Recount Methylation ", class(gri)[1]),
                                   version)
@@ -494,7 +494,7 @@ make.h5se <- function(newfn, dsn.data1, dsn.data2 = NULL,
   } else{
     if(verbose){message("Making GenomicMethylSet...")}
     gri <- minfi::GenomicMethylSet(gr = grcg,
-                                   Meth = ldat[[1]], 
+                                   Meth = ldat[[1]],
                                    Unmeth = ldat[[2]],
                                    anno = anno)
     metadata(gri) <- get_metadata(paste0("Recount Methylation ", class(gri)[1]),
@@ -505,11 +505,11 @@ make.h5se <- function(newfn, dsn.data1, dsn.data2 = NULL,
   # save seh5 object and finally run process
   t1 <- Sys.time()
   if(verbose){message("Saving seh5 object...")}
-  HDF5Array::saveHDF5SummarizedExperiment(gri.pheno, 
-                                          dir = "rmseh5_grnoob", 
+  HDF5Array::saveHDF5SummarizedExperiment(gri.pheno,
+                                          dir = "rmseh5_grnoob",
                                           replace = replace.opt)
   if(verbose){
-    message("Save complete. Process duration was ", 
+    message("Save complete. Process duration was ",
             Sys.time() - t1)
   }
   return(TRUE)
