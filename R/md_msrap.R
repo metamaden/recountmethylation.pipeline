69b59af48615051a80aab178cbab26135f0c80ee
Sean <maden@ohsu.edu>
2021-03-03 13:10:09 -0800

    adds get_msrap, snakemake script, modifies man

--- R/md_msrap.R
+++ R/md_msrap.R
@@ -1,50 +1,51 @@
+#!/usr/bin/env R
+
+# Author: Sean Maden
+# 
+# Get the MetaSRA-pipeline output data as a flat table.
+#
 
 # get mapped terms from metasra-pipeline
-get_msrap <- function(files.dir = "recount-methylation-files", 
-                      eq.dname = "equery", msrap.dname = "gsm_msrap_outfiles"){
-  # get paths
-  eqdpath = file.path(files.dir, eq.dname) # equery dir path
-  eq.dpath <- get_eqfpath(eqdpath) # equery file path
-  eq.fn <- gsub(".*/", "", eq.fpath) # equery file name
-  msrap.dpath <- file.path(files.dir, msrap.dname) # msrap dir path
-  
-  # get collapsed gse ids
-  x <- scan(eq.path, what="", sep="\n");dat1 <- unlist(strsplit(x, " "))
-  gsmt <- data.frame(gsm = dat1[2:length(dat1)]);gsmt$gse <- dat1[1]
-  for(i in x[2:length(x)]){
-    dati <- unlist(strsplit(i, " "))
-    gsmti <- data.frame(gsm = dati[2:length(dati)])
-    gsmti$gse <- dati[1];gsmt <- rbind(gsmt, gsmti)}
-  
-  # collapse on repeat gsm/multiple gse id's
-  gsev <- unlist(lapply(gsmt[,1], function(x){
-    paste0(gsmt[gsmt[,1] == x, 2], collapse=";")}))
-  gsmt$gseid <- gsev
-  
-  # filter vars, repeated values
-  gmap <- gsmt[!duplicated(gsmt$gsm), c(1, 2)]
-  
-  # get msrap mapped terms
-  # append collapsed mapped metadata
-  gsmid.fnindex <- 3; gmap$msrap_flatjson <- "NA"
-  lfl.filt <- list.files(dpath)
-  for(f in lfl.filt){
+
+#' Get the pipeline data files as a flat table
+#'
+#' Reads data in the sample/GSM-level metadata files produced by the pipeline 
+#' and "map_msrap.py" script. Data is read and stored as a flat table ready to
+#' be appended to other metadata for the instance.
+#' 
+#' @param ts NTP timestamp for the new metadata table (string).
+#' @param files.dir Files dir for the instance ("recount-methylation-files").
+#' @param md.dname Metadata dir name, located in the files.dir ("metadata").
+#' @param msrap.dname Name of dir, located in the files.dir, containing 
+#' pipeline outputs for samples/GSM IDs ("gsm_msrap_outfiles").
+#' @param msrap.regex.str Regex pattern string for valid metadata pipeline 
+#' output files ("^msrapout.*").
+#' @param gsmid.fnindex File name index of the GSM ID for valid metadata 
+#' pipeline output files (2).
+#' @param gmap.fn File name stem of the new mapped metadata table 
+#' ("md_msrapout").
+#' @return NULL, outputs the mapped data table to the metadata directory.
+#' @export
+get_msrap <- function(ts, files.dir = "recount-methylation-files", md.dname = "metadata",
+                      msrap.dname = "gsm_msrap_outfiles", msrap.regex.str = "^msrapout.*",
+                      gsmid.fnindex = 2, gmap.fn = "md_msrapout"){
+  rpath <- file.path(files.dir, msrap.dname)
+  wpath <- file.path(files.dir, md.dname, paste(gmap.fn, ts, collapse = "."))
+  rl <- list.files(rpath); rl <- rl[grepl(msrap.regex.str, rl)]
+  gmap <- matrix(nrow = 0, ncol = 2);lfl.filt <- list.files(rpath)
+  for(f in rl){
     gsmi <- unlist(strsplit(f,"\\."))[gsmid.fnindex]
-    if(gsmi %in% gmap[,1]){
-      jfi <- jsonlite::fromJSON(txt = file.path(dpath, f), flatten=T)
-      xi <- jfi[,!colnames(jfi) %in% c("mapped ontology terms", "real-value properties")]
-      pi <- paste0("'",names(xi),"':'",as.character(xi[1,]),"'",collapse=";")
-      if("real-value properties" %in% names(jfi)){
-        if(length(unlist(jfi[,"real-value properties"]))>0){
-          pi <- paste0(pi,";'real-value properties':",
-                       as.character(unlist(jfi[,"real-value properties"])), 
-                       collapse="")}}
-      mot <- paste0(as.character(unlist(jfi[,"mapped ontology terms"])), collapse=";")
-      jfi.mapped.flat <- paste0(pi, ";", mot)
-      gmap[gmap[,1]==gsmi,]$msrap_flatjson <- jfi.mapped.flat}}
-  # save data
-  gmap.fn <- "mdmap-gsm"
-  save(gmap, file=paste0(gmap.fn, ".rda"))
-  write.csv(gmap, file=paste0(gmap.fn, ".csv"))
-  return(NULL)
-}
+    jfi <- jsonlite::fromJSON(txt = file.path(dpath, f), flatten=T)
+    xi <- jfi[,!colnames(jfi) %in% 
+                c("mapped ontology terms", "real-value properties")]
+    pi <- paste0("'",names(xi),"':'",as.character(xi[1,]),"'",collapse=";")
+    if("real-value properties" %in% names(jfi)){
+      if(length(unlist(jfi[,"real-value properties"]))>0){
+        pi <- paste0(pi,";'real-value properties':",
+                     as.character(unlist(jfi[,"real-value properties"])),
+                     collapse="")}}
+    mot <- paste0(as.character(unlist(jfi[,"mapped ontology terms"])), collapse=";")
+    jfi.mapped.flat<-paste0(pi,";",mot)
+    gmapi<-matrix(c(gsmi,jfi.mapped.flat),nrow=1);gmap <- rbind(gmap, gmapi)}
+  gmap <- as.data.frame(gmap, stringsAsFactors = FALSE)
+  colnames(gmap) <- c("gsm", "msrapout");save(gmap, file = wpath);return(NULL)}
---
+++ inst/snakemake/do_msrap.R
@@ -0,0 +1,10 @@
+#!/usr/bin/env R
+
+# Author: Sean Maden
+# 
+# Snakemake script to read the MetaSRA-pipeline mapped metadata into 
+# a flat table.
+
+library(recount.synth)
+
+get_msrap()
